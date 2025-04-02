38ad43cd3686c311f74123a2aff3a0edf0003fff
Henrique Godinho <ola@hgod.in>
2025-04-02 17:33:48 -0300

    python script

---
+++ build.py
@@ -0,0 +1,32 @@
+
+
+def redirector():
+    html = '<html><head><meta http-equiv="refresh" content="0;url={url}" /></head></html>'
+    
+    with open("redirects.json") as f:
+        redirects = json.load(f)
+        
+        for redirect in redirects:
+            
+            html_file = html.format(url=redirect["url"])
+            name = redirect["path"]
+            file_path = f"./dist/{name}.html"
+            
+            with open(file_path, "w") as file:
+                file.write(html_file)
+                
+if __name__ == "__main__": 
+    import json
+    import os
+    
+    # Clean up the dist directory
+    if os.path.exists("./dist"):
+        import shutil
+        shutil.rmtree("./dist")
+        
+    # Create the dist directory
+    os.makedirs("./dist")
+    
+    redirector()
+    
+    print("Redirector HTML files created successfully.")
